998132385d48789f511f1bee60212903344d55b4
lim0n3 <simonestaffa96@gmail.com>
2024-02-29 16:04:20 -0700

    update campaign image

--- app/api/campaigns/[id]/route.ts
+++ app/api/campaigns/[id]/route.ts
@@ -1,50 +1,47 @@
 import { authOptions } from "@/app/lib/auth";
 import { getCampaignById, updateCampaign } from "@/lib/db/campaign";
+import { getUserById, updateUser, upsertUser } from "@/lib/db/user";
+import { uploadImage } from "@/lib/imagekit";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-): Promise<NextResponse> {
-  const { id } = params;
+export const PUT = async (req: Request) => {
   const session = await getServerSession(authOptions);
 
   if (!session) {
     return new NextResponse("Unauthorized", { status: 401 });
   }
-  const {
-    name,
-    description,
-    githubRepoUrl,
-    githubRepoId,
-    websiteUrl,
-    imageUrl,
-  } = await req.json();
+  const data = await req.formData();
+  const file: File | null = data.get("file") as unknown as File;
 
-  if (
-    !name ||
-    !description ||
-    !githubRepoUrl ||
-    !githubRepoId ||
-    !websiteUrl ||
-    !imageUrl
-  ) {
-    return new NextResponse("Missing required fields", { status: 422 });
+  if (!file) {
+    return Response.json({ success: false });
   }
 
-  const userId = session.user.id.toString();
-  const campaign = await updateCampaign(parseInt(id, 10), userId, {
-    name,
-    description,
-    githubRepoUrl,
-    githubRepoId,
-    websiteUrl,
-    imageUrl,
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  // Retrieve the Profile address
+  const user = await getUserById(session.user.id.toString());
+
+  if (!user) {
+    // If the profile doesn't exist, return a 404 Not Found response
+    return Response.json({ result: "Profile doesn't exist." }, { status: 404 });
+  }
+
+  const imageUpload = await uploadImage(
+    buffer,
+    `${user.address.toLowerCase()}_avatar`
+  );
+
+  // Update the profile with the provided data
+  const updatedProfile = await updateUser(user.address, {
+    image: imageUpload.url,
   });
 
-  return NextResponse.json(campaign);
-}
+  // Return a 200 OK response with the updated profile
+  return Response.json(updatedProfile, { status: 200 });
+};
 
 export const GET = async (
   req: NextRequest,
--- lib/db/user.ts
+++ lib/db/user.ts
@@ -1,19 +1,20 @@
 import { prisma } from "../prisma";
 
 export const upsertUser = async (
-  address: string,
+  id: string,
   data: {
     address?: string;
     name?: string;
-    avatarUrl?: string;
+    image?: string;
   }
 ) => {
   return await prisma.user.upsert({
-    where: { address },
+    where: { id },
     update: data,
     create: {
-      address,
-      ...data,
+      address: data.address!,
+      name: data.name,
+      image: data.image,
     },
   });
 };
@@ -23,7 +24,7 @@ export const createUser = async (
   data: {
     address: string;
     name: string;
-    avatarUrl: string;
+    image: string;
   }
 ) => {
   return await upsertUser(address, data);
@@ -33,16 +34,16 @@ export const updateUser = async (
   address: string,
   data: {
     name?: string;
-    avatarUrl?: string;
+    image?: string;
   }
 ) => {
   return await upsertUser(address, data);
 };
 
-export const getUserById = async (id: number) => {
+export const getUserById = async (id: string) => {
   return await prisma.user.findUnique({ where: { id } });
 };
 
 export const getUserByAddress = async (address: string) => {
-  return await prisma.user.findUnique({ where: { address } });
+  return await prisma.user.findFirst({ where: { address } });
 };
