6402f09f243a8e809366ef8a504f26669c20c7cc
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-03-01 06:36:50 +0100

    add contract address

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,4 +7,6 @@ export const BASE_SCAN_URL = "https://basescan.org";
 export const ACROSS_ADDRESS_POLYGON =
   "0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096";
 
-export const PAPABASE_ADDRESS = "";
+export const PAPABASE_BASE_ADDRESS = ""
+
+export const PAPABASE_BASE_SEPOLIA_ADDRESS = "0xb137CF186E6c32B97E20F5aBD294e47Ee95E8Ac1"
