a56b439e06c4dfe604373932f0e8eb4d2716c0e9
PaoloRollo <paolo.rollo1997@gmail.com>
2024-03-01 22:18:58 -0700

    feat: update.

--- lib/db/user.ts
+++ lib/db/user.ts
@@ -1,49 +1,5 @@
 import { prisma } from "../prisma";
 
-export const upsertUser = async (
-  id: string,
-  data: {
-    address?: string;
-    name?: string;
-    imageUrl?: string;
-  }
-) => {
-  return await prisma.user.upsert({
-    where: { id },
-    update: data,
-    create: {
-      address: data.address!,
-      name: data.name,
-      imageUrl: data.imageUrl,
-    },
-  });
-};
-
-export const createUser = async (
-  id: string,
-  data: {
-    address: string;
-    name: string;
-    imageUrl: string;
-  }
-) => {
-  return await upsertUser(id, data);
-};
-
-export const updateUser = async (
-  id: string,
-  data: {
-    name?: string;
-    imageUrl?: string;
-  }
-) => {
-  return await upsertUser(id, data);
-};
-
 export const getUserById = async (id: string) => {
   return await prisma.user.findUnique({ where: { id } });
-};
-
-export const getUserByAddress = async (address: string) => {
-  return await prisma.user.findFirst({ where: { address } });
 };
